eee2d7cdc400ccd57cabae53413ce41aab353dc6
vitushynskyi1 <vitushynskyi1@uniba.sk>
2025-11-29 00:41:29 +0100

    enums

---
+++ src/main/java/CardsData.java
@@ -0,0 +1,40 @@
+public class CardsData {
+    private CardsData() {}
+
+    public static class GridPosition {
+        private byte x ;
+        private byte y;
+        public GridPosition(int x, int y) throws IllegalArgumentException {
+            if(x < -2 || x > 2 || y < -2 || y > 2) throw new IllegalArgumentException("Position (" + x + "," + y + ") is out of bounds of grid.");
+            this.x = (byte)x;
+            this.y = (byte)y;
+        }
+
+        public int getX() {
+            return (int)x;
+        }
+
+        public int getY() {
+            return (int)y;
+        }
+    }
+
+    public static class CardSource {
+        private final byte index; 
+        private final GameObjects.Deck source;
+        
+        public CardSource(int index, GameObjects.Deck source) {
+            this.index = (byte)index;
+            this.source = source;
+        }
+
+        public int getIndex() {
+            return (int)index;
+        }
+
+        public GameObjects.Deck getSourceDeck() {
+            return source;
+        }
+        
+    }
+}
