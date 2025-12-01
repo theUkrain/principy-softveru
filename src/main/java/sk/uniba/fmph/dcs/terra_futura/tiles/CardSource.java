6a834d3d82c718791c2dbaeb1df5336f43c8449b
theUkrain <nikitarozhkovv@gmail.com>
2025-12-01 15:27:14 +0100

    Post pull fix

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/tiles/CardSource.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/tiles/CardSource.java
@@ -1,6 +1,5 @@
 package sk.uniba.fmph.dcs.terra_futura.tiles;
 import sk.uniba.fmph.dcs.terra_futura.ConstantGameObjects.Deck;
-import sk.uniba.fmph.dcs.terra_futura.ConstantGameObjects.Resource;
 
 public  class CardSource {
     private final byte index;
