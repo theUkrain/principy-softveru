714ce448fed9a5f3032fbc7154a34e420f36aca8
denPrytula <denispritula1010@gmail.com>
2025-12-01 13:04:26 +0100

    Add EffectTest

---
+++ src/test/java/sk/uniba/fmph/dcs/terra_futura/EffectsTest.java
@@ -0,0 +1,12 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class EffectsTest {
+    @Test
+    @DisplayName("TransformationFixed test")
+    public void testTransformationFixed() {
+
+    }
+}
