a31ff83bf6cdfef1b2edb6795cab3f4d972d71c8
denPrytula <denispritula1010@gmail.com>
2025-12-01 21:04:36 +0100

    Exchange for Nikita

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/effects/Exchange.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/effects/Exchange.java
@@ -5,6 +5,7 @@ import sk.uniba.fmph.dcs.terra_futura.ConstantGameObjects.Resource;
 import sk.uniba.fmph.dcs.terra_futura.tiles.Card;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,39 +27,44 @@ public class Exchange extends SetCardToEffect {
      * @param input <<Resource, Amount>, Taken from this card>.
      * @param output <Resource, Amount> expected to get.
      */
+    public int execute (Map<Resource,Pair<Card, Integer>> input, Set<Pair<Resource, Integer>> output) {
+        Set<Pair<Resource, Integer>> kostily = new HashSet();
 
-    public int execute (Map<Pair<Resource, Integer>, Card> input, Set<Pair<Resource, Integer>> output) {
+        for (Map.Entry<Resource, Pair<Card, Integer>> entry : input.entrySet()) {
+            kostily.add(Pair.of(entry.getKey(), entry.getValue().getRight()));
+        }
 
-        if(outputs.contains(output)) {
-            throw new UnsupportedOperationException("Effect with possible outputs: \n" + outputs.toString() +
+        if(this.outputs.contains(output)) {
+            throw new UnsupportedOperationException("Effect with possible outputs: \n" + this.outputs.toString() +
                     "\n doesn't support output: " + output.toString());
         }
 
-        if(inputs.contains(inputs)) {
-            throw new UnsupportedOperationException("Effect with possible inputs: \n" + inputs.toString() +
-                    "\n doesn't support input: " + input.toString());
+       if(this.inputs.contains(kostily)) {
+            throw new UnsupportedOperationException("Effect with possible inputs: \n" + this.inputs.toString() +
+                    "\n doesn't support input: " + kostily.toString());
+       }
+
+       for (Map.Entry<Resource, Pair<Card, Integer>> entry : input.entrySet()) {
+            Resource resourceToTake = entry.getKey();
+            Card card = entry.getValue().getLeft();
+            Integer amount = entry.getValue().getRight();
+
+            Map<Resource, Integer> requestDetails = Map.of(resourceToTake, amount);
+
+            if (!(card.canGetResources(requestDetails))) {
+                throw new IllegalArgumentException(
+                        "Card: \n " + card.toString() + " can't provide \n" + resourceToTake + " x" + amount);
+            }
         }
+       for (Map.Entry<Resource, Pair<Card, Integer>> entry : input.entrySet()) {
+           Resource resourceToTake = entry.getKey();
+           Card card = entry.getValue().getLeft();
+           Integer amount = entry.getValue().getRight();
 
+           Map<Resource, Integer> request = Map.of(resourceToTake, amount);
 
-
-        for(Pair<Resource, Integer> resourcesRequest : input.keySet()) {
-
-            Map<Resource, Integer> requestDetails = Map.of(resourcesRequest.getKey(), resourcesRequest.getValue());
-
-            if(!(input.get(resourcesRequest).canGetResources(requestDetails)))
-                throw new IllegalArgumentException
-                        ("Card: \n " + input.get(resourcesRequest).toString() + "can't provide \n" + resourcesRequest);
+           card.getResources(request);
         }
-
-
-        for(Pair<Resource, Integer> resourcesRequest : input.keySet()) {
-
-            Map<Resource, Integer> requestDetails = Map.of(resourcesRequest.getKey(), resourcesRequest.getValue());
-
-            input.get(resourcesRequest).getResources(requestDetails);
-        }
-
-
 
         Map<Resource, Integer> resourcesToPut = new HashMap<>();
 
@@ -85,7 +91,7 @@ public class Exchange extends SetCardToEffect {
 
     @Override
     public String toString() {
-        return "Exchange effect can provide exchange of one of following inputs to one of outputs: \n Inputs:\n" + inputs.toString() +
-                "\n Outputs: " + outputs.toString();
+        return "Exchange effect can provide exchange of one of following inputs to one of outputs: \n Inputs:\n" + this.inputs.toString() +
+                "\n Outputs: " + this.outputs.toString();
     }
 }
