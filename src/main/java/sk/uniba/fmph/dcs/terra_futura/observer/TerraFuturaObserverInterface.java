a01317a2145c44dceebee704a7c1ec6978e05297
denPrytula <denispritula1010@gmail.com>
2025-12-03 00:19:46 +0100

    Observer Update

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/observer/TerraFuturaObserverInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/observer/TerraFuturaObserverInterface.java
@@ -1,9 +1,15 @@
 package sk.uniba.fmph.dcs.terra_futura.observer;
 
+// input output to connect + method from input take string + method output string
 /**
  * Interface for observers that receive game state notifications.
  */
 public interface TerraFuturaObserverInterface {
 
     void notify(String gameState);
+
+    String read();
+
+    void write(String message);
+
 }
